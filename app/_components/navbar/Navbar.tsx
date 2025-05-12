0b028cdac445e67da0e90ef23b64cedf8a8fc2fa
Surya Prakash Mallah <sfrost488@gmail.com>
2025-05-12 18:50:42 +0530

    navbar fix

--- app/_components/navbar/Navbar.tsx
+++ app/_components/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { DialogTrigger } from "@radix-ui/react-dialog";
+import { DialogDescription, DialogTrigger } from "@radix-ui/react-dialog";
 import { Menu, Pencil } from "lucide-react";
 import { useState } from "react";
 import EditProfileBox from "../profile/EditProfileColumn";
@@ -15,26 +15,24 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="h-16 flex justify-between items-center px-4">
-      <div className="font-extrabold text-3xl">Portfoli<span className="text-green-700">u</span>m</div>
+      <div className="font-extrabold text-3xl">
+        Portfoli<span className="text-green-700">u</span>m
+      </div>
       <div className="space-x-2.5">
         <Dialog>
           <DialogTrigger asChild>
             <Button size="icon">
-              <Pencil/>
-            </Button >
+              <Pencil />
+            </Button>
           </DialogTrigger>
           <DialogContent>
             <DialogHeader>
               <DialogTitle>Edit Profile</DialogTitle>
+              <DialogDescription className="sr-only"></DialogDescription>
             </DialogHeader>
-
-
 
             {/* Profile container for editing */}
             <EditProfileBox />
-
-
-
           </DialogContent>
         </Dialog>
 
