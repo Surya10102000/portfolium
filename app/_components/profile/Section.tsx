f46a52be246d02a55148034c5760c0d1bfed0ffd
Surya Prakash Mallah <sfrost488@gmail.com>
2025-06-06 22:50:14 +0530

    fix : section option

--- app/_components/profile/Section.tsx
+++ app/_components/profile/Section.tsx
@@ -1,27 +1,41 @@
 import { ArrowRightIcon } from "lucide-react";
 import React from "react";
 
-interface SectionsProps{
-    title : string;
-    description : string;
-    icon : React.ReactNode;
-    onClick : () => void;
+interface SectionsProps {
+  title: string;
+  description: string;
+  icon: React.ReactNode;
+  onClick: () => void;
 }
 
-const Section : React.FC<SectionsProps> = ({title, description , icon, onClick}) => {
+const Section: React.FC<SectionsProps> = ({
+  title,
+  description,
+  icon,
+  onClick,
+}) => {
   return (
-    <div className="flex justify-between items-center gap-4 border p-4 rounded-lg cursor-pointer hover:bg-gray-50 transition-colors" onClick={onClick}>
-      <div>
-        {icon}
+    <div
+      className="flex justify-between items-center gap-4 border p-4 rounded-md cursor-pointer
+                 transition-colors duration-200 ease-in-out
+                 "
+      onClick={onClick}
+      role="button"
+      tabIndex={0}
+      onKeyDown={(e) => e.key === "Enter" && onClick()}
+    >
+      <div className="text-card-foreground">{icon}</div>
+
+      <div className="flex-1">
+        <p className="font-medium ">{title}</p>
+        <p className="text-sm">{description}</p>
       </div>
-      <div className="flex-1 ">
-        <p>{title}</p>
-        <p>{description}</p>
-      </div>
+
       <div>
-        <ArrowRightIcon />
+        <ArrowRightIcon className="h-5 w-5" />
       </div>
     </div>
   );
 };
+
 export default Section;
