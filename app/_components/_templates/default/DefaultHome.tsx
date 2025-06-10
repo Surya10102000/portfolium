a82f037c9e44b3b7cd728514e42e9b725569be95
Surya Prakash Mallah <sfrost488@gmail.com>
2025-06-10 16:51:32 +0530

    added hero Section

--- app/_components/_templates/default/DefaultHome.tsx
+++ app/_components/_templates/default/DefaultHome.tsx
@@ -2,13 +2,14 @@
 import { UserData } from "@/types/userData";
 import { aboutIsEmpty, arrayIsEmpty } from "../templateUtils";
 import { mockHeroData, mockUserData } from "@/public/mockData";
+import HeroSection from "./components/HeroSection";
 const DefaultHome = ({ data }: { data: UserData }) => {
   // const { hero, about, education, projects, experience, primaryColor } = data;
   const {hero , about ,education} = mockUserData
 
   return (
-    <div className="">
-      <div className="h-screen bg-amber-300" id="hero">hero</div>
+    <div>
+      <HeroSection {...hero}/>
       <div className="h-screen bg-red-400" id="project">project</div>
       <div className="h-screen bg-green-600" id="education">education</div>
       <div className="h-screen bg-teal-500" id="experience">experience</div>
---
+++ app/_components/_templates/default/components/HeroSection.tsx
@@ -0,0 +1,8 @@
+import { HeroSectionI } from "@/types/userData"
+
+const HeroSection = ({name, description, image,role} : HeroSectionI) => {
+  return (
+    <div className="h-[80lvh]" id="hero">hero</div>
+  )
+}
+export default HeroSection
