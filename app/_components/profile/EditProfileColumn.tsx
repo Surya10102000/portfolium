424e94f2a1da8ac69e5132a851ade950321ba893
Surya Prakash Mallah <sfrost488@gmail.com>
2025-06-14 01:01:42 +0530

    fix : EditProfileColumn

--- app/_components/profile/EditProfileColumn.tsx
+++ app/_components/profile/EditProfileColumn.tsx
@@ -78,7 +78,7 @@ const EditProfileBox = () => {
   ];
 
   return (
-    <div className="flex flex-col gap-2 overflow-y-scroll scrollable-content h-[88vh] px-4` ">
+    <div className="flex flex-col gap-2 overflow-y-scroll scrollable-content h-[88vh]` ">
       {sections.map((section) => (
         <Section
           key={section.id}
