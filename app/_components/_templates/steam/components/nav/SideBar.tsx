9f38439d858b8b10f8431dbbd43a6d211effddd5
Surya Prakash Mallah <sfrost488@gmail.com>
2025-06-18 15:37:38 +0530

    added steamTemplate sidebar

---
+++ app/_components/_templates/steam/components/nav/SideBar.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import styles from "./sidebar.module.scss";
+import { motion } from "framer-motion";
+import { UserData } from "@/types/userData";
+import { getNonEmptySections } from "../../../templateUtils";
+import { cn } from "@/lib/utils";
+
+export const SideBar = ({ portfolioData }: { portfolioData: UserData }) => {
+  const sectionOption = getNonEmptySections(portfolioData).filter(
+    (a) => a !== "hero"
+  );
+  const name = portfolioData.hero.name as string;
+  const [selected, setSelected] = useState("");
+
+  useEffect(() => {
+    const sections = document.querySelectorAll(".section-wrapper");
+
+    const options = {
+      threshold: 0.3,
+    };
+
+    const callback = (entries: any) => {
+      entries.forEach((entry: any) => {
+        if (entry.isIntersecting) {
+          setSelected(entry.target.id);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    sections.forEach((section) => observer.observe(section));
+  }, []);
+
+  return (
+    <div style={{ background: "var(--background-dark)" }}>
+      <motion.nav
+        initial={{ x: -70 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.5 }}
+        className={styles.sideBar}
+      >
+        <span
+          className={styles.logo}
+          onClick={() => {
+            document.location.hash === ""
+              ? document.getElementById("main")?.scrollIntoView()
+              : (document.location.hash = "");
+          }}
+        >
+          {name.slice(0, 2)}
+          <span>.</span>
+        </span>
+
+        {sectionOption.map((sec, i) => (
+          <motion.a
+            key={i}
+            initial={{ x: -70 }}
+            animate={{ x: 0 }}
+            transition={{ duration: 0.5, delay: 0.1 }}
+            href={`#${sec}`}
+            onClick={() => {
+              setSelected(sec);
+            }}
+            className={cn(
+              "capitalize",
+              selected === sec ? styles.selected : ""
+            )}
+          >
+            {sec}
+          </motion.a>
+        ))}
+      </motion.nav>
+    </div>
+  );
+};
