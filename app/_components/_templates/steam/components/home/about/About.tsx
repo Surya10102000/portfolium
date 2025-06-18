ff1e72b6ca7b26ea88e22c420a07e0ee76c920e7
Surya Prakash Mallah <sfrost488@gmail.com>
2025-06-18 20:57:27 +0530

    added steamTemplate about section

---
+++ app/_components/_templates/steam/components/home/about/About.tsx
@@ -0,0 +1,48 @@
+import { ArrowUpRightFromSquare, ChevronRight } from "lucide-react";
+import { Reveal } from "../../utils/Reveal";
+import { SectionHeader } from "../../utils/SectionHeader";
+import styles from "./about.module.scss";
+import { Stats } from "./Stats";
+import { MyLinks } from "../../nav/components/MyLinks";
+import { AboutSection, Contact } from "@/types/userData";
+import { cn } from "@/lib/utils";
+import { arrayIsEmpty } from "@/app/_components/_templates/templateUtils";
+
+export const About = ({
+  about,
+  contact,
+}: {
+  about: AboutSection;
+  contact: Contact;
+}) => {
+  const {aboutMe,whatIDo,techStack} = about;
+  return (
+    <section id="about" className="section-wrapper">
+      <SectionHeader title="About" dir="l" />
+      <div className={styles.about}>
+        <div>
+          <Reveal>
+            <p className={`${styles.aboutText} ${styles.highlightFirstLetter}`}>
+              {aboutMe}
+            </p>
+          </Reveal>
+          <Reveal>
+            <p className={styles.aboutText}>
+              {whatIDo}
+            </p>
+          </Reveal>
+          <Reveal>
+            <div className={styles.links}>
+              <div className={cn(styles.linksText, "mt-4")}>
+                <span>My links</span>
+                <ChevronRight />
+              </div>
+              <MyLinks contact={contact} />
+            </div>
+          </Reveal>
+        </div>
+        {techStack?.length !== 0 && <Stats techStack={techStack!}/>}
+      </div>
+    </section>
+  );
+};
---
+++ app/_components/_templates/steam/components/home/about/Stats.tsx
@@ -0,0 +1,23 @@
+import { Smile, SquareCode } from "lucide-react";
+import { Reveal } from "../../utils/Reveal";
+import styles from "./stats.module.scss";
+
+export const Stats = ({techStack} : {techStack : string[]}) => {
+  return (
+    <div className={styles.stats}>
+      <Reveal>
+        <div className={styles.statColumn}>
+          <h4>
+            <SquareCode size="2.4rem" color="var(--brand)" />
+            <span>Use at work</span>
+          </h4>
+          <div className={styles.statGrid}>
+            {techStack.map((tech, i )=>(
+            <span key={i} className="chip">{tech}</span>
+            ))}
+          </div>
+        </div>
+      </Reveal>
+    </div>
+  );
+};
