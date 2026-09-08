2febc404f45d28dfe40f111690d9a4fb82ff01db
Surya10102000 <suryaprakashmallah@gmail.com>
2026-09-08 15:38:19 +0530

    refactor: add options to ReactLenis for improved scroll performance

--- app/_components/_templates/luminary/layout.tsx
+++ app/_components/_templates/luminary/layout.tsx
@@ -15,7 +15,7 @@ interface LayoutProps {
 const LuminaryLayout = ({ children, userData }: LayoutProps) => {
   return (
     <ThemeProvider>
-      <ReactLenis root/>
+      <ReactLenis options={{ lerp: 0.07, wheelMultiplier: 0.9 }}root/>
       <div className={`mx-auto tracking-tight flex flex-col justify-between`}>
         <div className="flex-1 px-5 md:px-7 lg:px-9 mx-auto tracking-tight">
           <nav className={`default-navbar`}>
